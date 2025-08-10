8528fb8b51165b3fe97168336d0db2c25dc27d62
luke <olianna.luxine@foxmail.com>
2025-08-11 04:19:00 +0800

    update

--- base/src/plugins/piniahook.ts
+++ base/src/plugins/piniahook.ts
@@ -3,7 +3,6 @@ import { PiniaPluginContext } from 'pinia';
 export const initHook = async (ctx: PiniaPluginContext) => {
   const { store } = ctx;
   if (typeof store.__init__ === 'function') await store.__init__();
-  debugger
   if (typeof store.__initState__ === 'boolean' && store.__initState__ === false) {
     store.__initState__ = true;
     console.log('initState');
